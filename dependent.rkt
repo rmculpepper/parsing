858b6b22d7b1bf9be973596d2f9709b4e759bb62
Ryan Culpepper <ryanc@racket-lang.org>
2019-07-09 18:49:05 +0200

    ... wip: dependent parsing

--- dependent.rkt
+++ dependent.rkt
@@ -9,28 +9,30 @@
 
 ;; Kinded Tokens and Dependent Grammars
 
-;; A TokenKind is a Symbol
-;; The default TokenKind is 'default
-;; The pseudo-terminal EOF may belong to multiple token kinds.
+;; A TokenKind is a Symbol.
+
+;; A Token has a default TokenKind, which is used by the parser to
+;; select the TokenReader for a set of expected Tokens. But a
+;; TokenReader may produce other Tokens. (?? limit to declared ??)
+
+;; The pseudo-terminal EOF may belong to multiple TokenKinds.
+
+;; ?? Useful to limit membership of TokenKind explicitly?
+;; ?? Useful to limit membership of TokenFunction results explicitly?
+
+;; A Tokenizer has the following interface:
+;; - read : (TokenKind -> Token) and (TokenFunction Any ... -> Token)
+;; - peek : (TokenKind -> Token) -- functions not supported ??
+
+;; ?? Don't allow functions in lookahead position?
 
 ;; An Alphabet is (alphabet Hash[TerminalSymbol => TokenKind])
-(struct alphabet (t=>kind) #:prefab)
+(struct alphabet (t=>kind fun=>arity) #:prefab)
 (struct dgrammar (start alpha defs) #:prefab)
 
-;; A Def is (def NT (Listof Item) (Values -> Result))
+;; A Def is (def NT (Listof Item) (... -> Result))
 ;; An Item is (Listof GSymbol)
 (struct def (nt rhss action) #:prefab)
-
-;; FIXME: need a way of sneaking a value into Token position, to allow eg
-;;   Msg ::= f:Flag (? (has-X-bit? f) X) Y
-;; But we need to avoid question of expression equality. Maybe allow
-;;   (? LimitedExpression) where LimitedExpression = DepVar | (Symbol DepVar)
-;; and assume that the symbol is a deterministic function? Or do
-;; identitifier comparison and replace with canonical (eq?)
-;; identifier? Or add TokenFunctions, eg
-;;   token function (has-X-bit? _) : Boolean
-;;   Msg :: f:Flag [true (has-X-bit? f)] X Y
-;;   Msg :: f:Flag [false (has-X-bit? f)] Y
 
 ;; In grammar specification, an Element can be
 ;;   NT                         -- short for [_ NT], if don't care about value
@@ -41,10 +43,38 @@
 ;;   [T : (TF Expr ...)]        -- short for [_ T : (TF Expr ...)], if we don't care about value
 ;;   [name T : (TF Expr ...)]   -- where Expr = name | (quote Datum)
 
-;; Maybe a Token doesn't have a unique TokenKind, there are just
-;; TokenReaders, and a Token has an optional default TokenReader.
-;; Then, for example, {true, false} could be shared by many TokenFunctions.
+;; During table construction, an Element is one of
+;; - (ntelem NT)
+;; - (telem T TReader)
+;;   where TReader is TKind or (list TFunction Expr ...)
+;;   where TKind and TFunction are Symbols
+;;   and Expr is Nat or (quote Datum) -- nat is stack index
+
+
+
+;; Some design rationale: We want a way of sneaking a value into Token
+;; position, to allow eg
+;;
+;;   Msg ::= f:Flag (if (has-X-bit? f) then X else ε) Y
+;;
+;; But we need to avoid (or limit) the question of expression
+;; equality, or else go from a "set of sequence" view of productions
+;; to an "expression" view. So we don't allow arbitrary expressions;
+;; instead, we require registration of functions and recognize them by
+;; symbol name, and we limit arguments to element variables and quoted
+;; constants. So for example:
+;;
+;;   token function (has-X-bit? _)
+;;   Msg -> [f Flag] [true  : (has-X-bit? f)] X Y
+;;   Msg -> [f Flag] [false : (has-X-bit? f)] Y
+
+
+;; FIXME: allow booleans, fixnums, and characters to be Tokens?
+;; Booleans would make the example above nicer.
+
 ;; Orthogonal: token may have payload, may not.
+
+;; Can a Tokenizer produce a nonterminal instance? Why not?
 
 ;; ============================================================
 
