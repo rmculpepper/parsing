7330b85cbf20ba4d7c60585c34fab7c22567e243
Ryan Culpepper <ryanc@racket-lang.org>
2019-10-27 10:45:10 +0100

    ... todo notes

--- main.rkt
+++ main.rkt
@@ -7,3 +7,15 @@
          define-grammar
          lr-parser
          ll1-parser)
+
+;; ----------------------------------------
+;; TO DO
+
+;; - better tokenizer support
+;; - source locations, real token structure
+;; - consistent conventions about absent payloads
+
+;; - conflicts should be errors by default
+;; - parser introspection (show states, dump debug table)
+;;   - accessible at run time interactively (not like parser-tools/yacc debug)
+;; - better parsing errors, hooks for users
