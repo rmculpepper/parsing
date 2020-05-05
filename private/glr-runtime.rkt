2c540ef36f1d4f3c38de6c1a72de5fdcfdae304c
Ryan Culpepper <ryanc@racket-lang.org>
2020-05-05 07:50:17 +0200

    glr-runtime: clean up stack types, conventions

--- private/glr-runtime.rkt
+++ private/glr-runtime.rkt
@@ -9,6 +9,8 @@
 ;; - null
 ;; - (cons X TStack)
 ;; - (TJoin (NonemptyListof TStack)) -- TJoin should not contain TJoin child
+;; A TStack represents a list of stacks with tail sharing (standard
+;; for cons-based lists) *and* head sharing (via TJoin).
 (struct TJoin (stacks) #:prefab)
 
 ;; tstack-look : TStack Nat -> (NonemptyListof TStack)
@@ -78,17 +80,29 @@
      (define trs (map pstate-tr (map car groups)))
      (if fail (fail trs) (error 'glr-parse "ambiguous token reader\n  candidates: ~e" trs))]))
 
-(define (with-tstack-pop-values tsk arity k)
-  ;;(check-state-at-top? 'with-stack-pop-values tsk)
-  (let loop ([tsk tsk] [arity arity] [acc null])
-    (cond [(zero? arity) (k tsk acc)]
-          [else (with-tstack-look tsk 2
-                  (lambda (s1 v2 tsk**) (loop tsk** (sub1 arity) (cons v2 acc))))])))
+;; ============================================================
+
+;; A StStack has a State at the top, followed by a VStack (or empty).
+
+;; A VStack has a Value (a token or reduced nonterminal) at the top,
+;; followed by a StStack.
+
+;; conventions:
+;; - sk, sk** : StStack
+;; - vsk*     : VStack
+
+(define (with-tstack-pop-values sk arity k)
+  ;;(check-state-at-top? 'with-stack-pop-values sk)
+  (let loop ([sk sk] [arity arity] [acc null])
+    (cond [(zero? arity) (k sk acc)]
+          [else (with-tstack-look sk 2
+                  (lambda (s1 v2 sk**) (loop sk** (sub1 arity) (cons v2 acc))))])))
 
 ;; ----------------------------------------
 
 (define (glr-parse states vals tz #:mode [mode 'complete])
   (define DEBUG? #f)
+  (define KEEP-FAIL? #f)
   (define-syntax-rule (push! var value) (set! var (cons value var)))
   (define (get-state n) (vector-ref states n))
   (define (get-val n) (vector-ref vals n))
@@ -100,80 +114,78 @@
       [(cons (? symbol? tk) '()) (tz #f tk null)]
       [_ (error 'glr-parse "unsupported token reader: ~e" tr)]))
 
-  (define failed null) ;; mutated; (Listof TStack)
-  (define ready null) ;; mutated; (Listof TStack); ready to look at next token
+  (define failed null) ;; mutated; (Listof VStack)
+  (define ready null) ;; mutated; (Listof StStack); ready to look at next token
   (define done null) ;; mutated; (Listof Result)
 
-  ;; run-until-look : runs and adds to ready
-  (define (run-until-look tsk next-tok)
-    ;;(check-state-at-top? 'run-until-look/1 tsk)
-    (with-tstack-look tsk 1
-      (lambda (st tsk*)
+  ;; run-until-look : StStack ?? -> Void
+  ;; runs and adds to ready
+  (define (run-until-look sk next-tok)
+    ;;(check-state-at-top? 'run-until-look/1 ssk)
+    (with-tstack-look sk 1
+      (lambda (st vsk*)
         (when DEBUG? (eprintf "\nSTATE = #~v, ~s\n" (pstate-index st) (pstate-label st)))
         (cond [(and (eq? (pstate-accept st) 'true))
                ;; we got here by a shift; discard shift and return state
-               ;;(check-value-at-top? 'run-until-look/2 tsk*)
-               (with-tstack-look tsk* 3
-                 (lambda (v1 s2 v3 tsk**) (push! done (tok-v v3))))]
+               ;;(check-value-at-top? 'run-until-look/2 vsk*)
+               (with-tstack-look vsk* 3
+                 (lambda (v1 s2 v3 sk**) (push! done (tok-v v3))))]
               [(and (eq? (pstate-accept st) 'virtual) (memq mode '(first-done)))
-               ;;(check-value-at-top? 'run-until-look/3 tsk*)
+               ;;(check-value-at-top? 'run-until-look/3 vsk*)
                ;; we got here by a goto; result is first value
-               (with-tstack-look tsk* 1
-                 (lambda (v1 tsk**) (push! done (tok-v v1))))]
+               (with-tstack-look vsk* 1
+                 (lambda (v1 sk**) (push! done (tok-v v1))))]
               [(pstate-lookahead st)
                => (lambda (lookahead)
-                    (cond [next-tok (look* st tsk* next-tok)]
-                          [else (push! ready (cons st tsk*))]))]
+                    (cond [next-tok (look* st vsk* next-tok)]
+                          [else (push! ready (cons st vsk*))]))]
               [else
                (for ([red (pstate-reduce st)])
-                 (reduce st tsk* red next-tok))
-               (cond [next-tok (look* st tsk* next-tok)]
+                 (reduce st vsk* red next-tok))
+               (cond [next-tok (look* st vsk* next-tok)]
                      [(not (hash-empty? (pstate-shift st)))
-                      (push! ready (cons st tsk*))])]))))
+                      (push! ready (cons st vsk*))])]))))
 
-  (define (reduce st tsk* red next-tok)
-    ;;(check-value-at-top? 'reduce/1 tsk*)
+  (define (reduce st vsk* red next-tok)
+    ;;(check-value-at-top? 'reduce/1 vsk*)
     (match-define (reduction nt index arity action) red)
-    (with-tstack-pop-values (cons st tsk*) arity
-      (lambda (tsk** args)
-        ;;(check-state-at-top? 'reduce/2 tsk**)
+    (with-tstack-pop-values (cons st vsk*) arity
+      (lambda (sk** args)
+        ;;(check-state-at-top? 'reduce/2 sk**)
         (define value (tok nt (apply (get-val action) args)))
         (when DEBUG? (eprintf "REDUCE: ~v\n" value))
-        (goto value tsk** next-tok))))
+        (goto value sk** next-tok))))
 
-  ;; convention:
-  ;; - tsk, tsk**, etc   -- TStack with a state index at top
-  ;; - tsk*, tsk***, etc -- TStack with a token at top
-
-  (define (look stacks)
-    (define tr (pstates-consistent-tr (map car stacks)))
+  (define (look sks) ;; sks : (Listof StStack), each stack starts with cons
+    (define tr (pstates-consistent-tr (map car sks)))
     (define next-tok (get-next-token tr))
-    (for ([stack (in-list stacks)])
-      (match-define (cons st tsk*) stack)
-      (look* st tsk* next-tok)))
-  (define (look* st tsk* next-tok)
-    ;;(check-value-at-top? 'shift/1 tsk*)
+    (for ([sk (in-list sks)])
+      (match-define (cons st vsk*) sk)
+      (look* st vsk* next-tok)))
+  (define (look* st vsk* next-tok)
+    ;;(check-value-at-top? 'shift/1 vsk*)
     (define reds (hash-ref (or (pstate-lookahead st) #hash()) (tok-t next-tok) null))
-    (for ([red (in-list reds)]) (reduce st tsk* red next-tok))
+    (for ([red (in-list reds)]) (reduce st vsk* red next-tok))
     (cond [(hash-ref (pstate-shift st) (tok-t next-tok) #f)
            => (lambda (next-state)
                 (when DEBUG? (eprintf "SHIFT ~v, #~s\n" next-tok next-state))
-                (run-until-look (list* (get-state next-state) next-tok st tsk*) #f))]
+                (run-until-look (list* (get-state next-state) next-tok st vsk*) #f))]
           ;; Accept pre-parsed non-terminals from the lexer too.
           [(hash-ref (pstate-goto st) (tok-t next-tok) #f)
            => (lambda (next-state)
                 (when DEBUG? (eprintf "SHIFT ~v, #~s\n" next-tok next-state))
-                (run-until-look (list* (get-state next-state) next-tok st tsk*) #f))]
-          [(null? reds) (push! failed (list* next-tok st tsk*))]))
+                (run-until-look (list* (get-state next-state) next-tok st vsk*) #f))]
+          [(null? reds)
+           (when KEEP-FAIL? (push! failed (list* next-tok st vsk*)))]))
 
-  (define (goto reduced tsk next-tok)
-    (with-tstack-look tsk 1
-      (lambda (st tsk*)
-        ;;(check-value-at-top? 'goto/1 tsk*)
+  (define (goto reduced sk next-tok)
+    (with-tstack-look sk 1
+      (lambda (st vsk*)
+        ;;(check-value-at-top? 'goto/1 vsk*)
         (when DEBUG? (eprintf "RETURN VIA #~s\n" (pstate-index st)))
         (define next-state (hash-ref (pstate-goto st) (car reduced)))
         (when DEBUG? (eprintf "GOTO ~v\n" next-state))
-        (run-until-look (list* (get-state next-state) reduced st tsk*) next-tok))))
+        (run-until-look (list* (get-state next-state) reduced st vsk*) next-tok))))
 
   (define (run-all-ready)
     (when DEBUG? (eprintf "\n==== STEP ====\n"))
