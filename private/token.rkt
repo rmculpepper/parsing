0e6f53054ed1cfd5b045a02ae1f1f8b37205ff70
Ryan Culpepper <ryanc@racket-lang.org>
2020-05-19 17:02:57 +0200

    change token representation

--- private/token.rkt
+++ private/token.rkt
@@ -16,71 +16,77 @@
 
 ;; A TokenValue is one of
 ;; - safe-terminal
-;; - (token Terminal (U Any no-value) StartLoc/#f EndLoc/#f)
+;; - (token:e Terminal StartLoc/#f EndLoc/#f)
+;; - (token:v Terminal Any StartLoc/#f EndLoc/#f)
 ;; {Start,End}Loc = any non-false value, eg Nat (don't have to be the same types!)
-(struct tk (t v s e) #:prefab #:reflection-name 'token)
-
-(define no-value (string->uninterned-symbol "∅"))
+(struct tke (t s e) #:prefab #:reflection-name 'etoken)
+(struct tkv (t v s e) #:prefab #:reflection-name 'vtoken)
 
 (define (safe-terminal? v)
   (and (ok-terminal? v) (not (boolean? v))))
 
 (define token
   (case-lambda
-    [(t) (if (safe-terminal? t) t (tk t no-value #f #f))]
-    [(t v) (tk t v #f #f)]
-    [(t v s e) (tk t v s e)]))
+    [(t) (token/no-value t)]
+    [(t v) (tkv t v #f #f)]
+    [(t v s e) (tkv t v s e)]))
 
 (define token/no-value
   (case-lambda
-    [(t) (if (safe-terminal? t) t (tk t no-value #f #f))]
-    [(t s e) (tk t no-value s e)]))
+    [(t) (if (safe-terminal? t) t (tke t #f #f))]
+    [(t s e) (if (or s e) (tke t s e) (token/no-value t))]))
 
 (define (token? v)
-  (or (tk? v) (safe-terminal? v)))
+  (or (tke? v) (tkv? v) (safe-terminal? v)))
 
 (define (token-name x)
   (match x
-    [(tk t _ _ _) t]
+    [(tke t _ _) t]
+    [(tkv t _ _ _) t]
     [(? safe-terminal?) x]))
 
 (define (token-value x)
   (define (bad) (error 'token-value "token contains no value\n  token: ~e" x))
   (match x
-    [(tk _ v _ _) (if (eq? v no-value) (bad) v)]
+    [(tkv _ v _ _) v]
+    [(? tke?) (bad)]
     [(? safe-terminal?) (bad)]
     [_ (raise-argument-error 'token-value "token?" 0 x)]))
 
 (define (token-value* x [default #f])
   (match x
-    [(tk _ v _ _) (if (eq? v no-value) default v)]
+    [(tkv _ v _ _) v]
+    [(? tke?) default]
     [(? safe-terminal?) default]))
 
-(define (token-with-value? x)
-  (match x
-    [(tk _ v _ _) (not (eq? v no-value))]
-    [_ #f]))
+(define (token-with-value? x) (tkv? x))
 
 (define (token-no-value? x)
   (and (token? x) (not (token-with-value? x))))
 
 (define (token-start x)
-  (match x [(tk _ _ s e) s] [_ #f]))
+  (match x
+    [(tke _ s e) s]
+    [(tkv _ _ s e) s]
+    [_ #f]))
 (define (token-end x)
-  (match x [(tk _ _ s e) e] [_ #f]))
+  (match x
+    [(tke _ s e) e]
+    [(tkv _ _ s e) e]
+    [_ #f]))
 
 (define (token-add-locations x s e)
   (match x
-    [(tk t v s0 e0) (if (or s0 e0) x (tk t v s e))]
-    [(? safe-terminal? t) (tk t no-value s e)]))
+    [(tke t s0 e0) (if (or s0 e0) x (tke t s e))]
+    [(tkv t v s0 e0) (if (or s0 e0) x (tkv t v s e))]
+    [(? safe-terminal? t) (if (or s e) (tke t s e) t)]))
 
 (define-match-expander -token
   (lambda (stx)
     (syntax-case stx ()
       [(_ t) #'(? token? (app token-name t))]
-      [(_ t v) #'(? token-with-value? (app token-name t) (app token-value v))]
-      [(_ t v s e) #'(? token-with-value? (app token-name t) (app token-value v)
-                        (app token-start s) (app token-end e))]))
+      [(_ t v) #'(tkv t v _ _)]
+      [(_ t v s e) #'(tkv t v s e)]))
   (make-variable-like-transformer #'token))
 
 (define-match-expander -token/no-value
