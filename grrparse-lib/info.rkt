2126e7c518d02d2e0c8d39b91b3eb67eb417bfda
Ryan Culpepper <ryanc@racket-lang.org>
2020-05-20 05:08:33 +0200

    add info files

---
+++ grrparse-lib/info.rkt
@@ -0,0 +1,12 @@
+#lang info
+
+;; pkg info
+
+(define version "0.1")
+(define collection "grrparse")
+(define deps '("base"))
+(define pkg-authors '(ryanc))
+
+;; collection info
+
+(define name "grrparse")
---
+++ grrparse/info.rkt
@@ -0,0 +1,17 @@
+#lang info
+
+;; pkg info
+
+(define version "0.1")
+(define collection "grrparse")
+(define deps '("base" "grrparse-lib" "rackunit-lib"))
+(define build-deps '("racket-doc" "scribble-lib" "grrparse-lib"))
+(define implies '("grrparse-lib"))
+(define pkg-authors '(ryanc))
+
+;; collection info
+
+(define name "grrparse")
+;;(define scribblings '(("scribblings/grrparse.scrbl" (multi-page))))
+;;(define compile-omit-paths '("examples"))
+;;(define test-omit-paths '("examples"))
